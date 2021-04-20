1f9f8bc957d58213d360094d743f4a10dcdf05a9
nurulyca <nycovering@gmail.com>
2021-04-20 22:07:16 +0700

    Updated second day.

--- cinema.py
+++ cinema.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
+from datetime import date
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from flask_bcrypt import Bcrypt
+from flask_jwt import JWT, jwt_required, current_identity
+import jwt
 import re
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY']='secret'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost:5432/cinema'
+
+connect_str = 'postgresql://postgres:postgres@localhost:5432/cinema'
+engine = create_engine(connect_str, echo=False)
 
 class Auditorium(db.Model):
     auditorium_id = db.Column(db.Integer, primary_key=True, index=True)
@@ -37,9 +47,9 @@ class Movie(db.Model):
 
 class ScheduledMovie(db.Model):
     scheduled_movie_id = db.Column(db.Integer, primary_key=True, index=True)
-    start_movie = db.Column(db.Integer, nullable=False)
-    end_movie = db.Column(db.Integer, nullable=False)
-    movie_price = db.Column(db.DECIMAL(asdecimal=False), nullable=False)
+    start_movie = db.Column(db.String, nullable=False)
+    end_movie = db.Column(db.String, nullable=False)
+    movie_price = db.Column(db.Integer, nullable=False)
     movie_id = db.Column(db.Integer, foreign_key=True, nullable=False)
     auditorium_id = db.Column(db.Integer, nullable=False)
 
@@ -49,25 +59,26 @@ class Customer(db.Model):
     customer_name = db.Column(db.String, nullable=False)
     customer_password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False)
-    movie_id = db.Column(db.Integer, nullable=False)
 
 class Booking(db.Model):
     booking_id = db.Column(db.Integer, primary_key=True, index=True)
-    seat_id = db.Column(db.Integer, nullable=False)
+    seat_id = db.Column(db.Integer, nullable=False, unique=True)
     customer_id = db.Column(db.Integer, foreign_key=True, nullable=False)
-    movie_id = db.Column(db.Integer, nullable=False)
+    scheduled_movie_id = db.Column(db.Integer, foreign_key=True, nullable=False)
     booking_status = db.Column(db.String, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
 class Wallet(db.Model):
     wallet_id = db.Column(db.Integer, primary_key=True, index=True)
-    total_amount = db.Column(db.Integer, nullable=False)
+    total_amount = db.Column(db.Integer, nullable=False, default=0)
     customer_id = db.Column(db.Integer, foreign_key=True, nullable=False)
 
 class WalletTransaction(db.Model):
     wallet_transaction_id = db.Column(db.Integer, primary_key=True, index=True)
     total_amount = db.Column(db.Integer, nullable=False)
     wallet_id = db.Column(db.Integer, foreign_key=True, nullable=False)
-    booking_id = db.Column(db.Integer, nullable=False)
+    booking_id = db.Column(db.Integer, nullable=True)
     transaction_status = db.Column(db.String, nullable=False)
 
 regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
@@ -80,22 +91,26 @@ def create_customer():
             'error': 'Bad Request',
             'message': 'Name or email or password not given'
         }), 400
+
     if len(data['customer_name']) < 2:
         return jsonify({
             'error': 'Bad Request',
             'message': 'Name must contain minimum of 2 letters'
         }), 400
+
     if (re.search(regex, data['customer_email'])) == None:
         return jsonify({
             'error': 'Bad Request',
             'message': 'Email is invalid!'
         }), 400
     
+    pw_hash = bcrypt.generate_password_hash(data.get('customer_password')).decode('UTF-8')
+
     cust = Customer(
         customer_name = data['customer_name'],
         customer_email = data['customer_email'],
-        customer_password = data['customer_password']
-    )
+        customer_password = pw_hash
+        )
     try:
         db.session.add(cust)
         db.session.commit()
@@ -104,11 +119,24 @@ def create_customer():
             'error' : 'Email has been taken.'
         }, 400
     
+    #creating new wallet at the same time while registering new customer
+    wall = Wallet(
+        customer_id = cust.customer_id
+    )
+    try:
+        db.session.add(wall)
+        db.session.commit()
+    except:
+        return {
+            'error' : 'Error in creating wallet.'
+        }, 400
+
     return{
         'customer_id' : cust.customer_id,
         'name' : cust.customer_name,
         'email' : cust.customer_email,
-        'password' : cust.customer_password
+        'password' : cust.customer_password,
+        'wallet_id': wall.wallet_id
     }, 201
 
 @app.route('/login_customer/', methods = ['POST'])
@@ -118,7 +146,7 @@ def login_customer():
         return jsonify({
             'error' : 'Bad Request',
             'message' : 'Email or password not given'
-        }), 400
+        }), 401
     try:
         cust = Customer.query.filter_by(customer_email = data['customer_email']).first_or_404()
     except:
@@ -126,19 +154,45 @@ def login_customer():
             "message" : "Wrong email or password!"
         }), 401
 
-    return jsonify ({
+    payload = {'customer_id' : cust.customer_id, 'customer_email' : cust.customer_email}
+    encoded_jwt = jwt.encode(payload, 'secret', algorithm="HS256")
+
+    if bcrypt.check_password_hash(cust.customer_password, data["customer_password"]):
+        return jsonify ({
             'customer_id' : cust.customer_id,
             'name' : cust.customer_name,
             'email' : cust.customer_email,
-            'password' : cust.customer_password
+            'password' : cust.customer_password,
+            'access_token' : encoded_jwt.decode('UTF-8')
             })
+    else:
+        return jsonify ({
+            "message" : "Wrong email or password!"
+        }), 401
 
 @app.route('/update_customer/<id>', methods = ['PUT'])
 def update_customer(id):
     data = request.get_json()
+
+    token = request.headers.get("access_token").encode('UTF-8')
+
+    try:
+        decoded_token = jwt.decode(token, 'secret', algorithm=["HS256"])
+    except jwt.exceptions.DecodeError:
+        return "Access token is invalid!"
+    
+    if not 'customer_id' in decoded_token and not 'customer_email' in decoded_token:
+        return jsonify({
+            'error' : 'Bad Request',
+            'message' : 'Access token is invalid!'
+        }), 401
+
     cust = Customer.query.filter_by(customer_id = id).first_or_404()
+
     if 'customer_password' in data:
-        cust.customer_password = data['customer_password']
+        pw_hash = bcrypt.generate_password_hash(data.get('customer_password')).decode('UTF-8')
+        cust.customer_password = pw_hash
+
     if 'customer_email' in data:
         if (re.search(regex, data['customer_email'])):
             cust.customer_email = data['customer_email']
@@ -147,6 +201,7 @@ def update_customer(id):
                 'error': 'Bad Request',
                 'message': 'Email is invalid!'
                 }), 400
+
     if 'customer_name' in data:
         if len(data['customer_name']) > 2:
             cust.customer_name = data['customer_name']
@@ -155,7 +210,9 @@ def update_customer(id):
             'error': 'Bad Request',
             'message': 'Name must contain minimum of 2 letters'
         }), 400
+
     db.session.commit()
+
     return jsonify({
             'customer_id' : cust.customer_id,
             'name' : cust.customer_name,
@@ -231,7 +288,7 @@ def add_movie_schedule():
         end_movie = data['end_movie'],
         movie_id = data['movie_id'],
         movie_price = data['movie_price'],
-        auditorium_id = data['auditorium_id']
+        auditorium_id = data['auditorium_id'],
         )
 
     db.session.add(schedule)
@@ -241,10 +298,9 @@ def add_movie_schedule():
         'start_time' : schedule.start_movie,
         'end_time' : schedule.end_movie,
         'price' : schedule.movie_price,
-        'movie_id' : schedule.movie_price,
-        'auditorium_id' : schedule.auditorium_id
+        'movie_id' : schedule.movie_id,
+        'auditorium_id' : schedule.auditorium_id,
     }, 201
-    # return "ok"
 
 @app.route('/edit_movie_schedule/<id>', methods = ['PUT'])
 def edit_movie_schedule(id):
@@ -264,14 +320,163 @@ def edit_movie_schedule(id):
         'start_time' : schedule.start_movie,
         'end_time' : schedule.end_movie,
         'price' : schedule.movie_price,
-        'movie_id' : schedule.movie_price,
+        'movie_id' : schedule.movie_id,
         'auditorium_id' : schedule.auditorium_id
         }, 201
 
-# @app.route('/list_movie/<date>')
-# def get_list_movie(date):
-#     schedule = Scheduled_Movie.query.filter_by(start_movie = date)
-#     return jsonify({
-        
-#     })
+@app.route('/list_movie/<date>')
+def get_list_movie(date):
+    all = []
+    with engine.connect() as connection:
+        qry = text("SELECT * FROM public.movie JOIN scheduled_movie USING (movie_id) WHERE start_movie=:date")
+        result = connection.execute(qry, date=date)
+        for item in result:
+            all.append({
+                'movie_id': item[0],
+                'title': item[1],
+                'duration' : item[2],
+                'published_year' : item[3],
+                'category' : item[4]
+            })
+    return jsonify(all)
+
+@app.route('/search_movie/<title>')
+def search_movie(title):
+    all = []
+    with engine.connect() as connection:
+        qry = text("SELECT * FROM movie JOIN scheduled_movie USING (movie_id) WHERE movie_name ILIKE'%{}%';".format(title))
+        result = connection.execute(qry)
+        for item in result:
+            all.append({
+                'title': item[1],
+                'duration' : item[2],
+                'release_year' : item[3],
+                'category' : item[4],
+                'start_time' : item[6],
+                'end_time' : item[7],
+                'price' : item[8]
+            })
+    return jsonify(all)
+
+@app.route('/top_up/<id>/', methods = ['PUT'])
+def top_up(id):
+    data = request.get_json()
+    top = Wallet.query.filter_by(wallet_id=id).first_or_404()
+
+    if 'total_amount' in data:
+        top.total_amount += data['total_amount']
+    
+    db.session.commit()
+
+    wallTrans = WalletTransaction(
+        wallet_id = id,
+        total_amount = data['total_amount'],
+        transaction_status = "Top up"
+    )
+    db.session.add(wallTrans)
+    db.session.commit()
+
+    return {
+        'wallet_id' : top.wallet_id,
+        'total_amount' : top.total_amount,
+        'customer_id' : top.customer_id,
+        'transaction_status' : wallTrans.transaction_status
+    }, 201
+
+@app.route('/buy_ticket/', methods=['POST'])
+def buy_ticket():
+    data = request.get_json()
+    if not 'seat_id' in data and not 'customer_id' in data and not 'scheduled_movie_id' in data and not 'booking_status' in data and not 'quantity' in data: 
+        return jsonify({
+            'error': 'Bad Request',
+            'message': 'Seat ID, customer ID, scheduled movie ID, quantity or booking status can not be empty'
+        }), 400
+
+    try:
+        schedule = ScheduledMovie.query.filter_by(scheduled_movie_id=data['scheduled_movie_id']).first_or_404()
+    except:
+        return "Movie within the ID does not exist"
+    
+    total_price = schedule.movie_price * data['quantity']
+
+    book = Booking(
+        seat_id = data['seat_id'],
+        customer_id = data['customer_id'],
+        scheduled_movie_id = data['scheduled_movie_id'],
+        booking_status = data['booking_status'],
+        total_price = total_price,
+        quantity = data['quantity']
+    )
+
+    db.session.add(book)
+    db.session.commit()
+    
+    with engine.connect() as connection:
+        all = []
+        qry = text("SELECT * FROM booking JOIN scheduled_movie USING (scheduled_movie_id) WHERE booking_id=:booking_id")
+        result = connection.execute(qry, booking_id=book.booking_id)
+        for item in result:
+            all.append({
+                'booking_id': item[1], 
+                'seat_id': item[2], 
+                'customer_id': item[3],
+                'booking_status': item[4],
+                'start_time' : item[7],
+                'end_time' : item[8],
+                'price' : item[9],
+                'auditorium_id' : item[11],
+                'quantity' : item[5],
+                'total_price' : item[6]
+        })
+    
+    return jsonify(all)
+
+@app.route('/pay_ticket/', methods = ['POST'])
+def pay_ticket():
+    data = request.get_json()
+    if not 'wallet_id' in data and not 'booking_id' in data:
+        return jsonify({
+            'error' : 'Bad Request',
+            'message' : 'Wallet ID or booking ID must be given'
+        }), 400
+    
+    pay = Booking.query.filter_by(booking_id = data['booking_id']).first_or_404()
+    wal = Wallet.query.filter_by(wallet_id = data['wallet_id']).first_or_404()
+
+    if pay.total_price <= wal.total_amount :
+        wal.total_amount -= pay.total_price
+        pay.booking_status = "Paid"
+
+    db.session.commit()
+
+    #creating transaction history
+    wallTrans = WalletTransaction(
+        total_amount = pay.total_price,
+        transaction_status = "Success payment",
+        wallet_id = wal.wallet_id
+    )
+
+    db.session.add(wallTrans)
+    db.session.commit()
+
+    return jsonify({
+        'booking_id' : pay.booking_id,
+        'booking_status' : pay.booking_status,
+        'total_saldo' : wal.total_amount,
+        'transaction_status' : wallTrans.transaction_status
+    })
+
+@app.route('/top_five/')
+def top_five():
+    all = []
+    with engine.connect() as connection:
+        qry = text("SELECT movie.movie_id, movie.movie_name, COUNT(movie.movie_id) AS total FROM booking INNER JOIN scheduled_movie using (scheduled_movie_id) INNER JOIN movie USING (movie_id) WHERE booking_status = 'Paid' GROUP BY movie.movie_id ORDER BY total desc limit 5")
+        result = connection.execute(qry)
+        for item in result:
+            all.append({
+                'movie_id': item[0],
+                'title' : item[1], 
+                'sold' : item[2]
+            })
+    return jsonify(all)
 
